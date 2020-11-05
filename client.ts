b2debfcf0f9408c43c1eea70699d0956c8da344b
Joel Gustafson <joelg@mit.edu>
2020-11-05 12:34:49 -0500

    added query param support to client

--- client.ts
+++ client.ts
@@ -14,37 +14,51 @@ const componentPattern = /^\[(.+)\]$/
 
 function makeURL(
 	route: string,
-	params?: { [key: string]: string | string[] }
+	params: { [key: string]: string | string[] }
 ): string {
 	const path: string[] = []
 
-	if (params !== undefined) {
-		for (const component of route.split("/")) {
-			if (restComponentPattern.test(component)) {
-				const [{}, param] = restComponentPattern.exec(component)!
-				const values = params[param]
-				if (Array.isArray(values)) {
-					for (const value of values) {
-						path.push(encodeURIComponent(value))
-					}
-				} else {
-					throw new Error(`Invalid URL rest parameter: ${param}`)
-				}
-			} else if (componentPattern.test(component)) {
-				const [{}, param] = componentPattern.exec(component)!
-				const value = params[param]
-				if (typeof value === "string") {
+	for (const component of route.split("/")) {
+		if (restComponentPattern.test(component)) {
+			const [{}, param] = restComponentPattern.exec(component)!
+			const values = params[param]
+			if (Array.isArray(values)) {
+				delete params[param]
+				for (const value of values) {
 					path.push(encodeURIComponent(value))
-				} else {
-					throw new Error(`Invalid URL parameter: ${param}`)
 				}
 			} else {
-				path.push(encodeURIComponent(component))
+				throw new Error(`Invalid URL rest parameter: ${param}`)
 			}
+		} else if (componentPattern.test(component)) {
+			const [{}, param] = componentPattern.exec(component)!
+			const value = params[param]
+			if (typeof value === "string") {
+				delete params[param]
+				path.push(encodeURIComponent(value))
+			} else {
+				throw new Error(`Invalid URL parameter: ${param}`)
+			}
+		} else {
+			path.push(encodeURIComponent(component))
 		}
 	}
 
-	return path.join("/")
+	const keys = Object.keys(params)
+	if (keys.length > 0) {
+		const query: string[] = []
+		for (const key of keys) {
+			const value = params[key]
+			if (typeof value === "string") {
+				query.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+			} else {
+				throw new Error(`Invalid URL query parameter: ${key}`)
+			}
+		}
+		return `${path.join("/")}?${query.join("&")}`
+	} else {
+		return path.join("/")
+	}
 }
 
 function parseHeaders(headers: Headers): Record<string, string> {
